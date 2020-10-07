2b6d9a5fa388e5089611c6d57daf100e404c6bbf
Dunieski Otano <dunieskior@gmail.com>
2020-10-06 22:33:14 -0400

    Class Structure, Main Method, Imports, Static ImportsInitial commit

---
+++ JavaCertification_1Z0-808/src/com/anotherpackage/Animal.java
@@ -0,0 +1,11 @@
+package com.anotherpackage;
+
+public class Animal {
+    public static void main(String[] args) {
+        System.out.println("Animal"); // prints animal
+    }
+
+    public void printResult(){
+        System.out.println("Animal class");
+    }
+}
