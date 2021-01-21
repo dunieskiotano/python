eb94de35d717a267d0379a9056c9d2bdd4ea6baf
Dunieski Otano <dunieskior@gmail.com>
2021-01-21 17:08:35 -0500

    Dictionary Challenge Soution

--- Dictionaries/dictChallenge.py
+++ Dictionaries/dictChallenge.py
@@ -8,6 +8,13 @@ personInfoDictionary = {}
 length = len(keysList)
  # 0,1,2,3,4,5,6
 for index in range(length):
+#   personInfoDictionary[keysList[0]] = valuesList[0]
+#   personInfoDictionary[keysList[1]] = valuesList[1]
+#   personInfoDictionary[keysList[2]] = valuesList[2]
+#   personInfoDictionary[keysList[3]] = valuesList[3]
+#   personInfoDictionary[keysList[4]] = valuesList[4]
+#   personInfoDictionary[keysList[5]] = valuesList[5]
+#   personInfoDictionary[keysList[6]] = valuesList[6]
     personInfoDictionary[keysList[index]] = valuesList[index]
     
 for key, value in personInfoDictionary.items():
