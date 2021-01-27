850b2e216aec992f63e094943fa5fa30d8d142db
Dunieski Otano <dunieskior@gmail.com>
2021-01-27 15:13:15 -0500

    Added insert functionality

---
+++ Database-Python/functions.py
@@ -0,0 +1,79 @@
+import mysql.connector, os
+from os import environ
+from mysql.connector import connect
+
+
+# conn = connect(
+#     host=environ.get('DB_URL'),
+#     user=environ.get('DB_USER'),
+#     password=environ.get('DB_PASSWORD'),
+#     database=environ.get('DB_NAME')
+# )
+ 
+
+def readStudentInfo():  
+    conn = connect(
+    host=environ.get('DB_URL'),
+    user=environ.get('DB_USER'),
+    password=environ.get('DB_PASSWORD'),
+    database=environ.get('DB_NAME')
+)  
+    try:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM student')
+        for row in cursor:
+            print(f'''
+            id................ {row[0]}
+            First Name........ {row[1]}
+            Last Name......... {row[2]}
+            Age............... {row[3]}
+            ''')
+        cursor.close() 
+        conn.close() 
+    except(Exception, mysql.connector.Error) as error:
+        print('Error while fetching data from MySQL', error)
+
+def insertStudentInfo(fname, lname, age, phone):
+    conn = connect(
+    host=environ.get('DB_URL'),
+    user=environ.get('DB_USER'),
+    password=environ.get('DB_PASSWORD'),
+    database=environ.get('DB_NAME')
+)
+    try:
+        cursor = conn.cursor()
+        cursor.execute(f"INSERT INTO student (fname, lname, age, phone) VALUES ('{fname}', '{lname}', {age}, '{phone}')")
+        conn.commit()
+        cursor.close() 
+        conn.close() 
+    except(Exception, mysql.connector.Error) as error:
+        print('Error while fetching data from MySQL', error)
+    readStudentInfo()
+
+# ******** For Windows **********
+# First install python-dotenv
+
+# from dotenv import load_dotenv
+# import os
+# from os import getenv
+# load_dotenv()
+
+# create .env file under the root directory/folder and add the environment variable
+
+# conn = connect(
+#     host=getenv('DB_NAME'),
+#     user=getenv('DB_USER'),
+#     password=getenv('DB_URL'),
+#     database=getenv('DB_PASSWORD')
+#     port=getenv('DB_PORT')
+# )
+# ****** End of Windows ********* 
+
+# ******** macOS ************
+# conn = connect(
+#     host= environ.get('DB_URL'),
+#     user=environ.get('DB_USER'),
+#     password=environ.get('DB_PASSWORD'),
+#     database=environ.get('DB_NAME')
+# )
+# ******* End of macOS ************
---
+++ Database-Python/main.py
@@ -0,0 +1,17 @@
+import os 
+from os import system 
+import functions as func 
+from colorama import Fore, Back, Style
+
+system('clear')
+
+func.readStudentInfo()
+
+# fname = input("What's your first name? >> ")
+# lname = input("What's your last name? >> ")
+# age= int(input("What's your age? >> "))
+# phone = input("What is your phone? >> ")
+
+# func.insertStudentInfo(fname, lname, age, phone)
+
+
